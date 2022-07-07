d229b11a3de14f4166dbd9443ae743b8f3c7ad7f
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-07 10:46:27 -0700

    .

--- react/classcomponents/prop-it-up/src/App.js
+++ react/classcomponents/prop-it-up/src/App.js
@@ -2,20 +2,15 @@ import './App.css';
 
 import PersonCard from './components/personCard';
 
-const people = [
-  {"firstName": "Fred", "lastName": "Herald", "age": 55, "hairColor": "Black"},
-  {"firstName": "Kevin", "lastName": "Bacon", "age": 22, "hairColor": "Red"},
-  {"firstName": "Amber", "lastName": "Heard", "age": 45, "hairColor": "Poop-Brown"},
-  {"firstName": "Carol", "lastName": "Baskin", "age": 32, "hairColor": "Blonde"}
-]
 
 
 function App() {
   return (
     <div className="App">
-      {people.map(person => {
-      return <PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor}/>})
-      }
+      <PersonCard firstName="Joe" lastName="Herald" age={48} hairColor="Black"/>
+      <PersonCard firstName="Kevin" lastName="Bacon" age={22} hairColor="Red"/>
+      <PersonCard firstName="Martha" lastName="Stewart" age={32} hairColor="Green"/>
+      <PersonCard firstName="Amber" lastName="Heard" age={45} hairColor="Poop Brown"/>
     </div>
   );
 }
--- react/classcomponents/prop-it-up/src/components/personCard.js
+++ react/classcomponents/prop-it-up/src/components/personCard.js
@@ -3,10 +3,11 @@ import React, {Component} from 'react';
 
 class PersonCard extends Component {
     render() {
+        const {firstName, lastName, age, hairColor} = this.props;
         return <div>
-            <h1>{this.props.lastName}, {this.props.firstName}</h1>
-            <p>Age: {this.props.age}</p>
-            <p>Hair Color: {this.props.hairColor}</p>
+            <h1>{lastName}, {firstName}</h1>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
         </div>
     }
 }
