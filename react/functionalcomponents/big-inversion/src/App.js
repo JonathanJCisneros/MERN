29508cbeda02cb52a5dff0773dc7cf32f38e744d
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-07 19:21:07 -0700

    big-inversion

---
+++ react/functionalcomponents/big-inversion/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import PersonCard from './components/personCard'
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName="Joe" lastName="Herald" age={48} hairColor="Black"/>
+      <PersonCard firstName="Kevin" lastName="Bacon" age={22} hairColor="Red"/>
+      <PersonCard firstName="Martha" lastName="Stewart" age={32} hairColor="Green"/>
+      <PersonCard firstName="Amber" lastName="Heard" age={45} hairColor="Poop Brown"/>
+    </div>
+  );
+}
+
+export default App;
