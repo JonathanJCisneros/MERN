65888e39ad322d8f7c29e0ee8767414627ccf849
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-12 10:35:04 -0700

    morning lecture

--- lectures/week2/api-demo/src/components/fetchDemo/FetchDisplay.jsx
+++ lectures/week2/api-demo/src/components/fetchDemo/FetchDisplay.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const FetchDisplay = () => {
+const FetchDisplay = (props) => {
+    const {pokemon} = props;
     return (
         <fieldset>
             <legend>FetchDisplay.jsx</legend>
+            {
+                pokemon?
+                <div>
+                    <h1>{pokemon.name}</h1>
+                    <img src={pokemon.sprites.front_default} alt="img" />
+                </div>:
+                <h1>Fetch a pokemon by clicking on button</h1>
+            }
         </fieldset>
     )
 }
--- lectures/week2/api-demo/src/views/FetchDemo.jsx
+++ lectures/week2/api-demo/src/views/FetchDemo.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import FetchDisplay from '../components/fetchDemo/FetchDisplay'
+import axios from 'axios'
 
 const FetchDemo = () => {
+
+    const [pokemon, setPokemon] = useState()
+
     const fetchPokemonwithThen = () => {
         fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
             .then(response => {
@@ -9,16 +13,53 @@ const FetchDemo = () => {
             })
             .then(jsonResponse => {
                 console.log(jsonResponse)
+                setPokemon(jsonResponse)
             })
-            .catch(error => console.log(error))
+            .catch(error => console.log("...catch" + error))
     }
+
+    const fetchPokemonwithAwait = async() =>{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/eevee`)
+        const jsonResponse = await response.json()
+        setPokemon(jsonResponse)
+    }
+
+    const fetchPokemonwithAwait2 = async() =>{
+        try{
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/eev`)
+            const jsonResponse = await response.json()
+            setPokemon(jsonResponse)
+        }
+        catch(err){
+            setPokemon()
+            alert("The pokemon is not available")
+        }
+    }
+
+    const fetchPokemonwithAxios = () =>{
+        axios.get(`https://pokeapi.co/api/v2/pokemon/snorlax`)
+        .then(response => setPokemon(response.data))
+        .catch(error => console.log(error))
+    }
+
+    const fetchPokemonwithAxiosAsync = async() =>{
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/snorlax`)
+        setPokemon(response.data)
+    }
+
     return (
         <fieldset>
             <legend>FetchDemo.jsx</legend>
             <button onClick={fetchPokemonwithThen}>Fetch with .then</button>
-
-
-            <FetchDisplay/>
+            <span>  |  </span>
+            <button onClick={fetchPokemonwithAwait}>Fetch with Await</button>
+            <span>  |  </span>
+            <button onClick={fetchPokemonwithAwait2}>Fetch with Await2</button>
+            <span>  |  </span>
+            <button onClick={fetchPokemonwithAxios}>Fetch with Axios</button>
+            <span>  |  </span>
+            <button onClick={fetchPokemonwithAxiosAsync}>Fetch with Axios Async</button>
+            <FetchDisplay pokemon={pokemon}/>
         </fieldset>
     )
 }
