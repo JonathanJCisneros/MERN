c179f3c78cc42ced446178d5fd41e3264549afaf
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-16 20:58:46 -0700

    fix

--- full_stack_mern/first-project/client/src/views/Detail.jsx
+++ full_stack_mern/first-project/client/src/views/Detail.jsx
@@ -3,13 +3,13 @@ import axios from 'axios'
 import {useParams} from 'react-router-dom'
 
 const Detail = (props) => {
-    const [person, setPerson] = useState();
+    const [person, setPerson] = useState({});
     const {id} = useParams();
 
     useEffect(() => {
         axios.get(`http://localhost:8000/api/people/${id}`)
             .then(res => setPerson(res.data))
-            .catch(err => console.log(err))
+            .catch(err => console.error(err))
     }, []);
 
     return (
