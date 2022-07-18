3cdbb7a94817be5401b2cec4f8b5407dac7588ce
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-18 10:18:46 -0700

    relocating files

--- full_stack_mern/productManager/server/server.js
+++ full_stack_mern/productManager/server/server.js
@@ -3,12 +3,12 @@ const cors = require('cors')
 const app = express();
 const port = 8000;
 
-require('./server/config/mongoose.config');
+require('./config/mongoose.config');
 
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-require('./server/routes/product.routes')(app);
+require('./routes/product.routes')(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
 
