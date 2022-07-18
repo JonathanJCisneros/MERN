dfa852c17da4e8d5180c564cdb4a03a9a2b05bdb
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-17 17:49:35 -0700

    .

--- full_stack_mern/productManager/client/src/views/Detail.jsx
+++ full_stack_mern/productManager/client/src/views/Detail.jsx
@@ -2,9 +2,10 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import { useParams, Link, useNavigate } from 'react-router-dom'
 
-const Detail = () => {
+const Detail = (props) => {
     const [product, setProduct] = useState({});
     const {_id} = useParams();
+    const {removeFromDom} = props;
 
     const navigate = useNavigate();
 
@@ -14,12 +15,20 @@ const Detail = () => {
             .catch(err => console.log(err))
     }, []);
 
+    const deleteProduct = (id) => {
+        axios.delete(`http://localhost:8000/api/product/${id}`)
+            .then(res => {
+                removeFromDom(id)})
+            .catch(err => console.log(err));
+    }
+
+
     return (
         <div>
             <h1>{product.title}</h1>
             <h3>${product.price}</h3>
             <p>{product.description}</p>
-            <p><Link to={`/${product._id}/edit`}>Edit</Link></p>
+            <p><Link to={`/${product._id}/edit`}>Edit</Link> | <a href="/products/" onClick={(e)=> deleteProduct(product._id)}>Delete</a></p>
             <button onClick={() => navigate("/products/")}>Home</button>
         </div>
     )
--- full_stack_mern/productManager/client/src/views/Update.jsx
+++ full_stack_mern/productManager/client/src/views/Update.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {useParams, useNavigate} from 'react-router-dom'
 
-const Update = () => {
+const Update = (props) => {
     const {_id} = useParams();
     const [title, setTitle] = useState("")
     const [price, setPrice] = useState("")
