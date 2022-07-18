cdac8d81702af5d9fa4e4e5fb5d43afa8b815c97
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-18 10:38:00 -0700

    reformat files

--- full_stack_mern/first-project/server/server.js
+++ full_stack_mern/first-project/server/server.js
@@ -3,11 +3,11 @@ const cors = require('cors');
 const app = express();
 const port = 8000
 
-require('./server/config/mongoose.config');
+require('./config/mongoose.config');
 
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-require('./server/routes/person.routes')(app);
+require('./routes/person.routes')(app);
 
 app.listen(port, () => console.log(`Listening to port: ${port}`))
